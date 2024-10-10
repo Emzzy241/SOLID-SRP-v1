5d9cdb5b8cdd51cd76b350bcdb5715e76d7e49d0
Emzzy241 <emzzyoluwole@gmail.com>
2024-10-10 08:50:31 +0100

    add LSP

--- Models/UserService.cs
+++ Models/UserService.cs
@@ -1,10 +1,12 @@
-// Implementing the S in SOLID
-// S stands for SRP; Solid Responsibility Principle
+// Implementing the L in SOLID
+// The Liskov Substitution Principle (LSP) states, "you should be able to use any derived class instead of a parent class and have it behave in the same manner without modification.". 
+// It ensures that a derived class does not affect the behavior of the parent class; in other words, a derived class must be substitutable for its base class.
 
-// The rule states that a class should only be changed for one reason
-// his means that every class or similar structure in your code should have only one job
+// This principle is just an extension of the Open Closed Principle, and we must ensure that newly derived classes extend the base classes without changing their behavior. 
 
-// Our class should not be like a Swiss knife wherein if one of them needs to be changed, the entire tool needs to be altered. It does not mean that your classes should only contain one method or property. There may be many members as long as they relate to a single responsibility.
+// I will explain this with a real-world example that violates LSP.
+// A father is a doctor, whereas his son wants to become a cricketer. So here, the son can't replace his father even though they belong to the same family hierarchy.
 
-// The Single Responsibility Principle gives us a good way of identifying classes at the design phase of an application, and it makes you think of all the ways a class can change. 
-// However, a good separation of responsibilities is done only when we have the full picture of how the application should work. Let us check this with an example.
+// Now jump into an example to learn how a design can violate LSP. Suppose we need to build an app to manage data using a group of SQL files text. 
+// Here we need to write functionality to load and save the text of a group of SQL files in the application directory. 
+// So we need a class that manages the load and keeps the text of a group of SQL files along with the SqlFile Class. 
--- Program.cs
+++ Program.cs
@@ -1,58 +1,139 @@
-public class UserService
+public class SqlFile
 {
-   public void Register(string email, string password)
+   public string FilePath {get;set;}
+   public string FileText {get;set;}
+   public string LoadText()
    {
-      if (!ValidateEmail(email))
-         throw new ValidationException("Email is not an email");
-         var user = new User(email, password);
-
-         SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject="HEllo foo" });
+      /* Code to read text from sql file */
    }
-   public virtual bool ValidateEmail(string email)
+   public string SaveText()
    {
-     return email.Contains("@");
-   }
-   public bool SendEmail(MailMessage message)
-   {
-     _smtpClient.Send(message);
+      /* Code to save text into sql file */
    }
 }
 
-// It looks fine, but it is not following SRP. The SendEmail and ValidateEmail methods have nothing to do with the UserService class. Let's refract it.
-
-
-public class UserService
+public class SqlFileManager
 {
-   EmailService _emailService;
-   DbContext _dbContext;
-   public UserService(EmailService aEmailService, DbContext aDbContext)
-   {
-      _emailService = aEmailService;
-      _dbContext = aDbContext;
-   }
-   public void Register(string email, string password)
-   {
-      if (!_emailService.ValidateEmail(email))
-         throw new ValidationException("Email is not an email");
-         var user = new User(email, password);
-         _dbContext.Save(user);
-         _emailService.SendEmail(new MailMessage("myname@mydomain.com", email) {Subject="Hi. How are you!"});
+   public List<SqlFile> lstSqlFiles {get;set}
 
-    }
+   public string GetTextFromFiles()
+   {
+      StringBuilder objStrBuilder = new StringBuilder();
+      foreach(var objFile in lstSqlFiles)
+      {
+         objStrBuilder.Append(objFile.LoadText());
+      }
+      return objStrBuilder.ToString();
+   }
+   public void SaveTextIntoFiles()
+   {
+      foreach(var objFile in lstSqlFiles)
+      {
+         objFile.SaveText();
+      }
+   }
 }
-   public class EmailService
+
+// After our leaders tells us what to do:
+
+public class ReadOnlySqlFile
+{
+   string FilePath { get; set; }
+   string FileText { get; set; }
+
+   public string LoadText()
    {
-      SmtpClient _smtpClient;
-   public EmailService(SmtpClient aSmtpClient)
-   {
-      _smtpClient = aSmtpClient;
-   }
-   public bool virtual ValidateEmail(string email)
-   {
-      return email.Contains("@");
-   }
-   public bool SendEmail(MailMessage message)
-   {
-      _smtpClient.Send(message);
+      // Code to read text from an SQL file
    }
 }
+public class SqlFileManager
+{
+   public List<SqlFile> lstSqlFiles {get;set}
+   public string GetTextFromFiles()
+   {
+      StringBuilder objStrBuilder = new StringBuilder();
+      foreach(var objFile in lstSqlFiles)
+      {
+         objStrBuilder.Append(objFile.LoadText());
+      }
+      return objStrBuilder.ToString();
+   }
+   public void SaveTextIntoFiles()
+   {
+      foreach(var objFile in lstSqlFiles)
+      {
+         //Check whether the current file object is read-only or not.If yes, skip calling it's
+         // SaveText() method to skip the exception.
+
+         if(! objFile is ReadOnlySqlFile)
+         objFile.SaveText();
+      }
+   }
+}
+// Here we altered the SaveTextIntoFiles() method in the SqlFileManager class to determine whether or not the instance is of ReadOnlySqlFile to avoid the exception. 
+// We can't use this ReadOnlySqlFile class as a substitute for its parent without altering the SqlFileManager code. 
+// So we can say that this design is not following LSP. Let's make this design follow the LSP. Here we will introduce interfaces to make the SqlFileManager class independent from the rest of the blocks.
+
+public interface IWritableSqlFile
+{
+   string LoadText();
+}
+
+public interface IReadableSqlFile
+{
+   void SaveText();
+}
+
+// Now we implement IReadableSqlFile through the ReadOnlySqlFile class that reads only the text from read-only files. 
+// We implement both IWritableSqlFile and IReadableSqlFile in a SqlFile class by which we can read and write files.
+
+public class ReadOnlySqlFile : IReadableSqlFile
+{
+   string FilePath { get; set; }
+   string FileText { get; set; }
+
+   public string LoadText()
+   {
+      // Code to read text from an SQL file
+   }
+}
+
+public class SqlFile: IWritableSqlFile,IReadableSqlFile
+{
+   public string FilePath {get;set;}
+   public string FileText {get;set;}
+   public string LoadText()
+   {
+      /* Code to read text from sql file */
+   }
+   public void SaveText()
+   {
+      /* Code to save text into sql file */
+   }
+}
+
+// Now the design of the SqlFileManager class becomes like this.
+
+public class SqlFileManager
+{
+   public string GetTextFromFiles(List<IReadableSqlFile> aLstReadableFiles)
+   {
+      StringBuilder objStrBuilder = new StringBuilder();
+      foreach(var objFile in aLstReadableFiles)
+      {
+         objStrBuilder.Append(objFile.LoadText());
+      }
+      return objStrBuilder.ToString();
+   }
+   public void SaveTextIntoFiles(List<IWritableSqlFile> aLstWritableFiles)
+   {
+      foreach(var objFile in aLstWritableFiles)
+      {
+         objFile.SaveText();
+      }
+   }
+}
+
+// Here the GetTextFromFiles() method gets only the list of instances of classes that implement the IReadOnlySqlFile interface. 
+// That means the SqlFile and ReadOnlySqlFile class instances. And the SaveTextIntoFiles() method gets only the list instances of the class that implements the IWritableSqlFiles interface, in this case, SqlFile instances. 
+// So now we can say our design is following the LSP. And we fixed the problem using the Interface segregation principle (ISP), identifying the abstraction and the responsibility separation method.
