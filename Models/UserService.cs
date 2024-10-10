5cd78ea6e858c9a91e3683c7861550bba977df0c
Emzzy241 <emzzyoluwole@gmail.com>
2024-10-10 08:30:34 +0100

    add OCP

--- Models/UserService.cs
+++ Models/UserService.cs
@@ -1,10 +1,12 @@
-// Implementing the S in SOLID
-// S stands for SRP; Solid Responsibility Principle
+// Implementing the O in SOLID
+// O stands for SRP; Open-closed Principle
+// The Open/closed Principle says, "A software module/class is open for extension and closed for modification."
 
-// The rule states that a class should only be changed for one reason
-// his means that every class or similar structure in your code should have only one job
+// Open Closed Principle
 
-// Our class should not be like a Swiss knife wherein if one of them needs to be changed, the entire tool needs to be altered. It does not mean that your classes should only contain one method or property. There may be many members as long as they relate to a single responsibility.
+// Here:
+// "Open for extension" means we must design our module/class so that the new functionality can be added only when new requirements are generated. 
+// "Closed for modification" means we have already developed a class, and it has gone through unit testing. We should then not alter it until we find bugs. 
+// As it says, a class should be open for extensions; we can use inheritance. OK, let's dive into an example.
 
-// The Single Responsibility Principle gives us a good way of identifying classes at the design phase of an application, and it makes you think of all the ways a class can change. 
-// However, a good separation of responsibilities is done only when we have the full picture of how the application should work. Let us check this with an example.
+// Suppose we have a Rectangle class with the properties Height and Width.
--- Program.cs
+++ Program.cs
@@ -1,58 +1,103 @@
-public class UserService
+// Suppose we have a Rectangle class with the properties Height and Width.
+
+
+public class Rectangle{
+   public double Height {get;set;}
+   public double Wight {get;set; }
+}
+
+// Our app needs to calculate the total area of a collection of Rectangles. Since we already learned the Single Responsibility Principle (SRP), we don't need to put the total area calculation code inside the rectangle. 
+// So here, I created another class for area calculation.
+
+public class AreaCalculator {
+   public double TotalArea(Rectangle[] arrRectangles)
+   {
+      double area;
+      foreach(var objRectangle in arrRectangles)
+      {
+         area += objRectangle.Height * objRectangle.Width;
+      }
+      return area;
+   }
+}
+
+// Hey, we did it. We made our app without violating SRP. No issues for now. But can we extend our app so that it can calculate the area of not only Rectangles but also the area of Circles? 
+// Now we have an issue with the area calculation issue because the way to calculate the circle area is different. Hmm. Not a big deal. We can change the TotalArea method to accept an array of objects as an argument.
+// We check the object type in the loop and do area calculations based on the object type.  
+
+public class Rectangle{
+   public double Height {get;set;}
+   public double Wight {get;set; }
+}
+public class Circle{
+   public double Radius {get;set;}
+}
+public class AreaCalculator
 {
-   public void Register(string email, string password)
+   public double TotalArea(object[] arrObjects)
    {
-      if (!ValidateEmail(email))
-         throw new ValidationException("Email is not an email");
-         var user = new User(email, password);
-
-         SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject="HEllo foo" });
-   }
-   public virtual bool ValidateEmail(string email)
-   {
-     return email.Contains("@");
-   }
-   public bool SendEmail(MailMessage message)
-   {
-     _smtpClient.Send(message);
+      double area = 0;
+      Rectangle objRectangle;
+      Circle objCircle;
+      foreach(var obj in arrObjects)
+      {
+         if(obj is Rectangle)
+         {
+            area += obj.Height * obj.Width;
+         }
+         else
+         {
+            objCircle = (Circle)obj;
+            area += objCircle.Radius * objCircle.Radius * Math.PI;
+         }
+      }
+      return area;
    }
 }
 
-// It looks fine, but it is not following SRP. The SendEmail and ValidateEmail methods have nothing to do with the UserService class. Let's refract it.
+// Wow. We are done with the change. Here we successfully introduced Circle into our app. We can add a Triangle and calculate its area by adding one more "if" block in the TotalArea method of AreaCalculator. 
+// But every time we introduce a new shape, we must alter the TotalArea method. So the AreaCalculator class is not closed for modification. 
+// How can we make our design to avoid this situation? Generally, we can do this by referring to abstractions for dependencies, such as interfaces or abstract classes, rather than using concrete classes. 
+// Such interfaces can be fixed once developed so the classes that depend upon them can rely upon unchanging abstractions. Functionality can be added by creating new classes that implement the interfaces. So let's refract our code using an interface.
 
-
-public class UserService
+public abstract class Shape
 {
-   EmailService _emailService;
-   DbContext _dbContext;
-   public UserService(EmailService aEmailService, DbContext aDbContext)
-   {
-      _emailService = aEmailService;
-      _dbContext = aDbContext;
-   }
-   public void Register(string email, string password)
-   {
-      if (!_emailService.ValidateEmail(email))
-         throw new ValidationException("Email is not an email");
-         var user = new User(email, password);
-         _dbContext.Save(user);
-         _emailService.SendEmail(new MailMessage("myname@mydomain.com", email) {Subject="Hi. How are you!"});
+   public abstract double Area();
+}
 
-    }
-}
-   public class EmailService
+// Inheriting from Shape, the Rectangle and Circle classes now look like this:  
+
+public class Rectangle: Shape
+{
+   public double Height {get;set;}
+   public double Width {get;set;}
+   public override double Area()
    {
-      SmtpClient _smtpClient;
-   public EmailService(SmtpClient aSmtpClient)
-   {
-      _smtpClient = aSmtpClient;
-   }
-   public bool virtual ValidateEmail(string email)
-   {
-      return email.Contains("@");
-   }
-   public bool SendEmail(MailMessage message)
-   {
-      _smtpClient.Send(message);
+      return Height * Width;
    }
 }
+public class Circle: Shape
+{
+   public double Radius {get;set;}
+   public override double Area()
+   {
+      return Radius * Radus * Math.PI;
+   }
+}
+C#
+Every shape contains its area with its way of calculation functionality, and our AreaCalculator class will become simpler than before.
+
+public class AreaCalculator
+{
+   public double TotalArea(Shape[] arrShapes)
+   {
+      double area=0;
+      foreach(var objShape in arrShapes)
+      {
+         area += objShape.Area();
+      }
+      return area;
+   }
+}
+
+// Now our code is following SRP and OCP both. Whenever you introduce a new shape by deriving from the "Shape" abstract class, you need not change the "AreaCalculator" class. Awesome. Isn't it?
