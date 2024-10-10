14d768eec34667a13449916f8a5d44195869459c
Emzzy241 <emzzyoluwole@gmail.com>
2024-10-10 08:26:10 +0100

    add SRP

--- Models/UserService.cs
+++ Models/UserService.cs
@@ -1,50 +1,10 @@
+// Implementing the S in SOLID
+// S stands for SRP; Solid Responsibility Principle
 
-// THe D in SOLID 
-// DIP
-// Dependency Inversion Principle
+// The rule states that a class should only be changed for one reason
+// his means that every class or similar structure in your code should have only one job
 
-// The Dependency Inversion Principle (DIP) states that high-level modules/classes should not depend on low-level modules/classes. First, both should depend upon abstractions. Secondly, abstractions should not rely upon details. Finally, details should depend upon abstractions.
+// Our class should not be like a Swiss knife wherein if one of them needs to be changed, the entire tool needs to be altered. It does not mean that your classes should only contain one method or property. There may be many members as long as they relate to a single responsibility.
 
-// High-level modules/classes implement business rules or logic in a system (application). Low-level modules/classes deal with more detailed operations; in other words, they may write information to databases or pass messages to the operating system or services.
-
-// A high-level module/class that depends on low-level modules/classes or some other class and knows a lot about the other classes it interacts with is said to be tightly coupled. When a class knows explicitly about the design and implementation of another class, it raises the risk that changes to one class will break the other. So we must keep these high-level and low-level modules/classes loosely coupled as much as possible. To do that, we need to make both of them dependent on abstractions instead of knowing each other. Let's start with an example.
-
-// Suppose we need to work on an error-logging module(class) that logs exception stack traces into a file. Simple, isn't it? The following classes provide the functionality to log a stack trace into a file. 
-
-
-// Client file exports data from many files to a database
-
-// Looks good. We sent our application to the client. But our client wants to store this stack trace in a database if an IO exception occurs. Hmm... OK, no problem. We can implement that too. Here we need to add one more class that provides the functionality to log the stack trace into the database and an extra method in ExceptionLogger to interact with our new class to log the stack trace.
-
-
-// Looks fine for now. But whenever the client wants to introduce a new logger, we must alter ExceptionLogger by adding a new method. Suppose we continue doing this after some time. In that case, we will see a fat ExceptionLogger class with a large set of practices that provide the functionality to log a message into various targets. Why does this issue occur? Because ExceptionLogger directly contacts the low-level classes FileLogger and DbLogger to log the exception. We need to alter the design so that this ExceptionLogger class can be loosely coupled with those classes. To do that, we need to introduce an abstraction between them so that ExcetpionLogger can contact the abstraction to log the exception instead of directly depending on the low-level classes.
-
-// Now our low-level classes need to implement this interface.
-
-
-// Now, we move to the low-level class's initiation from the ExceptionLogger class to the DataExporter class to make ExceptionLogger loosely coupled with the low-level classes FileLogger and EventLogger. And by doing that, we are giving provision to DataExporter class to decide what kind of Logger should be called based on the exception that occurs.
-
-// We successfully removed the dependency on low-level classes. This ExceptionLogger doesn't depend on the FileLogger and EventLogger classes to log the stack trace. We no longer need to change the ExceptionLogger's code for the new logging functionality. We must create a new logging class that implements the ILogger interface and adds another catch block to the DataExporter class's ExportDataFromFile method.
-
-
-
-// And we need to add a condition in the DataExporter class as in the following:
-
-// Looks good. But we introduced the dependency here in the DataExporter class's catch blocks. So, someone must be responsible for providing the necessary objects to the ExceptionLogger to get the work done.
-
-// Let me explain it with a real-world example. Suppose we want to have a wooden chair with specific measurements and the kind of wood to be used to make that chair. Then we can't leave the decision-making on measures and the wood to the carpenter. Here his job is to make a chair based on our requirements with his tools, and we provide the specifications to him to make a good chair.
-
-// So what is the benefit we get from the design? Yes, we definitely have benefited from it. We need to modify the DataExporter and ExceptionLogger classes whenever we need to introduce a new logging functionality. But in the updated design, we need to add only another catch block for the new exception logging feature. We only need to properly understand the system, requirements, and environment and find areas where DIP should be followed. 
-// Coupling is not inherently evil. If you don't have some amount of coupling, your software will not do anything for you.
-
-// Conclusion
-// Great, we have gone through all five SOLID principles successfully. And we can conclude that using these principles, we can build an application with tidy, readable, and easily maintainable code.
-
-// Here you may have some doubts. Yes, about the quantity of code. Because of these principles, the code might become larger in our applications. But my dear friends, you need to compare it with the quality we get by following these principles. Hmm, but anyway, 27 lines are much fewer than 200 lines. 
-
-// Conclusion
-// Great, we have gone through all five SOLID principles successfully. And we can conclude that using these principles, we can build an application with tidy, readable, and easily maintainable code.
-
-// Here you may have some doubts. Yes, about the quantity of code. Because of these principles, the code might become larger in our applications. But my dear friends, you need to compare it with the quality we get by following these principles. Hmm, but anyway, 27 lines are much fewer than 200 lines. 
-
-// Orale
+// The Single Responsibility Principle gives us a good way of identifying classes at the design phase of an application, and it makes you think of all the ways a class can change. 
+// However, a good separation of responsibilities is done only when we have the full picture of how the application should work. Let us check this with an example.
--- Program.cs
+++ Program.cs
@@ -1,177 +1,58 @@
-// The following classes provide the functionality to log a stack trace into a file. 
-
-public class FileLogger
+public class UserService
 {
-   public void LogMessage(string aStackTrace)
+   public void Register(string email, string password)
    {
-      //code to log stack trace into a file.
+      if (!ValidateEmail(email))
+         throw new ValidationException("Email is not an email");
+         var user = new User(email, password);
+
+         SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject="HEllo foo" });
+   }
+   public virtual bool ValidateEmail(string email)
+   {
+     return email.Contains("@");
+   }
+   public bool SendEmail(MailMessage message)
+   {
+     _smtpClient.Send(message);
    }
 }
-public class ExceptionLogger
+
+// It looks fine, but it is not following SRP. The SendEmail and ValidateEmail methods have nothing to do with the UserService class. Let's refract it.
+
+
+public class UserService
 {
-   public void LogIntoFile(Exception aException)
+   EmailService _emailService;
+   DbContext _dbContext;
+   public UserService(EmailService aEmailService, DbContext aDbContext)
    {
-      FileLogger objFileLogger = new FileLogger();
-      objFileLogger.LogMessage(GetUserReadableMessage(aException));
+      _emailService = aEmailService;
+      _dbContext = aDbContext;
    }
-   private GetUserReadableMessage(Exception ex)
+   public void Register(string email, string password)
    {
-      string strMessage = string.Empty;
-      //code to convert Exception's stack trace and message to user readable format.
-    //   ....
-    //   ....
-      return strMessage;
+      if (!_emailService.ValidateEmail(email))
+         throw new ValidationException("Email is not an email");
+         var user = new User(email, password);
+         _dbContext.Save(user);
+         _emailService.SendEmail(new MailMessage("myname@mydomain.com", email) {Subject="Hi. How are you!"});
+
+    }
+}
+   public class EmailService
+   {
+      SmtpClient _smtpClient;
+   public EmailService(SmtpClient aSmtpClient)
+   {
+      _smtpClient = aSmtpClient;
+   }
+   public bool virtual ValidateEmail(string email)
+   {
+      return email.Contains("@");
+   }
+   public bool SendEmail(MailMessage message)
+   {
+      _smtpClient.Send(message);
    }
 }
-
-// Client file exports data from many files to a database
-public class DataExporter
-{
-   public void ExportDataFromFile()
-   {
-   try {
-      //code to export data from files to the database.
-   }
-   catch(Exception ex)
-   {
-      new ExceptionLogger().LogIntoFile(ex);
-   }
-}
-}
-
-
-
-
-// But our client wants to store this stack trace in a database if an IO exception occurs. Hmm... 
-public class DbLogger
-{
-   public void LogMessage(string aMessage)
-   {
-      //Code to write message in the database.
-   }
-}
-public class FileLogger
-{
-   public void LogMessage(string aStackTrace)
-   {
-      //code to log stack trace into a file.
-   }
-}
-public class ExceptionLogger
-{
-   public void LogIntoFile(Exception aException)
-   {
-      FileLogger objFileLogger = new FileLogger();
-      objFileLogger.LogMessage(GetUserReadableMessage(aException));
-   }
-   public void LogIntoDataBase(Exception aException)
-   {
-      DbLogger objDbLogger = new DbLogger();
-      objDbLogger.LogMessage(GetUserReadableMessage(aException));
-   }
-   private string GetUserReadableMessage(Exception ex)
-   {
-      string strMessage = string.Empty;
-      //code to convert Exception's stack trace and message to user readable format.
-      ....
-      ....
-      return strMessage;
-   }
-}
-
-public class DataExporter
-{
-   public void ExportDataFromFile()
-   {
-      try {
-         //code to export data from files to database.
-      }
-      catch(IOException ex)
-      {
-         new ExceptionLogger().LogIntoDataBase(ex);
-      }
-      catch(Exception ex)
-      {
-         new ExceptionLogger().LogIntoFile(ex);
-      }
-   }
-}
-
-// Now our low-level classes need to implement this interface
-.
-public interface ILogger
-{
-   void LogMessage(string aString);
-}
-
-public class DbLogger: ILogger
-{
-   public void LogMessage(string aMessage)
-   {
-      //Code to write message in database.
-   }
-}
-public class FileLogger: ILogger
-{
-   public void LogMessage(string aStackTrace)
-   {
-      //code to log stack trace into a file.
-   }
-}
-
-
-
-
-
-public class ExceptionLogger
-{
-   private ILogger _logger;
-   public ExceptionLogger(ILogger aLogger)
-   {
-      this._logger = aLogger;
-   }
-   public void LogException(Exception aException)
-   {
-      string strMessage = GetUserReadableMessage(aException);
-      this._logger.LogMessage(strMessage);
-   }
-   private string GetUserReadableMessage(Exception aException)
-   {
-      string strMessage = string.Empty;
-      //code to convert Exception's stack trace and message to user readable format.
-      ....
-      ....
-      return strMessage;
-   }
-}
-public class DataExporter
-{
-   public void ExportDataFromFile()
-   {
-      ExceptionLogger _exceptionLogger;
-      try {
-         //code to export data from files to database.
-      }
-      catch(IOException ex)
-      {
-         _exceptionLogger = new ExceptionLogger(new DbLogger());
-         _exceptionLogger.LogException(ex);
-      }
-      catch(Exception ex)
-      {
-         _exceptionLogger = new ExceptionLogger(new FileLogger());
-         _exceptionLogger.LogException(ex);
-      }
-   }
-}
-
-
-public class EventLogger: ILogger
-{
-   public void LogMessage(string aMessage)
-   {
-      //Code to write a message in system's event viewer.
-   }
-}
-
-// 6
